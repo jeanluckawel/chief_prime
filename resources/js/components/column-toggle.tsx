ffe1ba10ee8ceb834e0e1e4fc18ed481b9da9a7b
Paul Kafanda PK <paulkaf84@gmail.com>
2025-08-21 16:33:46 +0200

    **Refactor column-toggle and optimize app-sidebar imports**
    - Simplified `onCheckedChange` logic in `column-toggle.tsx` by removing unnecessary double negation
    - Removed unused `FileTextIcon` import in `app-sidebar.tsx`
    
    Signed-off-by: Paul Kafanda PK <paulkaf84@gmail.com>

--- resources/js/components/column-toggle.tsx
+++ resources/js/components/column-toggle.tsx
@@ -43,7 +43,7 @@ export function DataTableViewOptions<TData>({
                                 key={column.id}
                                 className="capitalize"
                                 checked={column.getIsVisible()}
-                                onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                                onCheckedChange={(value) => column.toggleVisibility(value)}
                             >
                                 {column.id}
                             </DropdownMenuCheckboxItem>
