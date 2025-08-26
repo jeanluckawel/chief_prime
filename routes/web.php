3f08dae00e08b4e660c5338c936be3bcaca46100
Paul Kafanda PK <paulkaf84@gmail.com>
2025-08-26 22:05:20 +0200

    **Add route for invoice detail view**
    - Added `invoices.show` route to `web.php` for accessing individual invoice details
    
    Signed-off-by: Paul Kafanda PK <paulkaf84@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name
 Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
 
 Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoices.index');
+Route::get('/invoices/{invoice}', [InvoiceController::class, 'show'])->name('invoices.show');
 Route::get('/invoices/create', [InvoiceController::class, 'create'])->name('invoices.create');
 Route::post('/invoices', [InvoiceController::class, 'store'])->name('invoices.store');
 
